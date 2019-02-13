e05d5d50e58cb856844106d67687f561ea083390
Max <private.kuehn@protonmail.com>
2019-02-13 15:00:11 +0100

    Updated Crosshair and finished aimAtCrosshair

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -28,8 +28,8 @@ ATank* ATankPlayerController::getControlledTank() const
 void ATankPlayerController::aimTowardsCrosshair()
 {
     if (!getControlledTank()) { return; }
-
     FVector hit_location;
+
     if (getSightRayHitLocation(hit_location))
     {
         UE_LOG(LogTemp, Warning, TEXT("Hit Location: %s"), *hit_location.ToString());
@@ -38,6 +38,52 @@ void ATankPlayerController::aimTowardsCrosshair()
 
 bool ATankPlayerController::getSightRayHitLocation(FVector& outhit_location) const
 {
-    outhit_location = FVector(1.0);
+    auto crosshair_screen_location = findCrosshairPosition();
+    FVector look_direction;
+
+    if (deprojectCrosshairPosition(crosshair_screen_location, look_direction))
+    {
+        getLookVectorHitLocation(look_direction, outhit_location);
+    }
+
     return true;
+}
+
+FVector2D ATankPlayerController::findCrosshairPosition() const
+{
+    int32 viewport_size_x, viewport_size_y;
+    GetViewportSize(viewport_size_x, viewport_size_y);
+    auto screen_location = FVector2D(viewport_size_x * crosshair_location_x_, viewport_size_y * crosshair_location_y_);
+    return screen_location;
+}
+
+bool ATankPlayerController::deprojectCrosshairPosition(
+    FVector2D &crosshair_screen_location, FVector &outworld_direction) const
+{
+    FVector world_location;
+
+    return DeprojectScreenPositionToWorld(
+            crosshair_screen_location.X, crosshair_screen_location.Y, world_location, outworld_direction);
+}
+
+bool ATankPlayerController::getLookVectorHitLocation(FVector look_direction, FVector &outhit_location) const
+{
+    FHitResult hit_result;
+    auto start_location = PlayerCameraManager->GetCameraLocation();
+
+    bool line_trace_success =
+    GetWorld()->LineTraceSingleByChannel(
+        hit_result,
+        start_location,
+        start_location + look_direction * line_trace_range_,
+        ECollisionChannel::ECC_Visibility
+    );
+
+    if (line_trace_success) {
+        outhit_location = hit_result.Location;
+    } else {
+        outhit_location = FVector(0.f);
+    }
+
+    return line_trace_success;
 }
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -22,7 +22,19 @@ public:
     virtual void Tick(float DeltaTime) override;
 
 private:
+    UPROPERTY(EditAnywhere)
+    float crosshair_location_x_ = 0.5;
+    UPROPERTY(EditAnywhere)
+    float crosshair_location_y_ = 0.3333;
+    UPROPERTY(EditAnywhere)
+    float line_trace_range_ = 1.e6;
+
     // Moves to Barrel so, that a shot would hit where the barrel intersects the world
     void aimTowardsCrosshair();
     bool getSightRayHitLocation(FVector& outhit_location) const;
+    bool getLookVectorHitLocation(FVector look_direction, FVector &outhit_location) const;
+
+    FVector2D findCrosshairPosition() const;
+
+    bool deprojectCrosshairPosition(FVector2D &crosshair_screen_location, FVector &outworld_direction) const;
 };
